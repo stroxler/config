eaaf8d6011cf59d67d271b13a8afff89fb5974e6
Steven Troxler <steven.troxler@berkeleyanalytics.com>
2014-08-18 15:11:59 -0700

    update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -3,13 +3,19 @@ apt-get install vim-gnome
 apt-get install git
 apt-get install meld
 apt-get install gitg
-apt-get install hdf5
+
 
 # core R packages
 apt-get install r-base-core
 apt-get install r-base-html
 apt-get install r-doc-pdf
 apt-get install r-doc-info
+
+# hdf5
+apt-get install libhdf5-7
+apt-get install libhdf5-dev
+apt-get install libhdf5-doc
+apt-get install hdfview # this also grabs java stuff
 
 # core python packages
 apt-get install python-dev 
