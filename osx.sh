22a2f7789e7b6c25e0af3da8592c09bfd7994448
Steven Troxler <steven.troxler@berkeleyanalytics.com>
2015-06-22 15:50:47 -0700

    Add to osx.sh

--- osx.sh
+++ osx.sh
@@ -1,2 +1,5 @@
 export JAVA_HOME=$(/usr/libexec/java_home)
 export TERMINAL_HOME=~/cl/config
+export JETTY_HOME=$HOME/cl/jetty-distribution-9.2.10.v20150310
+export TERMINAL_CORE_LOGS=/var/log/terminal-core
+export GRADLE_HOME=/usr/local/Cellar/gradle/2.3/libexec/
