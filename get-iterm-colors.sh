0643c062de1ca9bf76b24e3afa1e6ae0af9eb7a7
Steven Troxler <steven.troxler@gmail.com>
2016-08-11 10:16:37 -0700

    Add recommended themes in iterm
    There are so many themes that it's good to record some ones
    I like

--- get-iterm-colors.sh
+++ get-iterm-colors.sh
@@ -4,3 +4,5 @@ git clone https://github.com/mbadolato/iTerm2-Color-Schemes iterm-colors
 
 echo "You have item colors. To use them, go to open profiles -> edit -> colors"
 echo "Then, in the lower-right click on color presets, scroll down to the bottom, and click import"
+echo "There are a lot of themes, many of them good."
+echo "Some recommended ones: Ciapre, ... (fill this as I go)"
